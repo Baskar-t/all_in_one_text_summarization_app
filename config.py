f2546c67606ef755cfd1d75555ffa18067ffbe4a
Baskar <baskar.mailbox@gmail.com>
2024-05-25 01:33:15 +0530

    "updated code" On branch master Changes to be committed: 	new file:   .gitignore 	new file:   app/__pycache__/__init__.cpython-311.pyc 	new file:   app/__pycache__/secrets.cpython-311.pyc 	new file:   app/secrets.py 	modified:   config.py 	modified:   requirements.txt

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 class Config:
@@ -7,3 +9,4 @@ class Config:
 
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+
